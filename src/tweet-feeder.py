2ec34a7f274dac7b2c4513f24964741894b5c911
JsonPy <jason.pyanowski@gmail.com>
2022-12-09 12:41:09 +0100

    TweetFeeder now based on streaming - damn fast

--- src/tweet-feeder.py
+++ src/tweet-feeder.py
@@ -1,11 +1,13 @@
 import json
 import argparse
+import time
 
+from elasticsearch.helpers import streaming_bulk
 from utils import es_connect, pg_connect
 from tqdm import tqdm
 
 
-def iterate(cursor, attributes, size=100):
+def iterate(cursor, attributes, size=1000):
     """
     An iterator that returns objects from a PostgreSQL database connection.
     The objects are turned into dictionaries of a certain shape defined by attributes. 
@@ -22,65 +24,77 @@ def iterate(cursor, attributes, size=100):
             yield obj
 
 
-# parse command line arguments
-parser = argparse.ArgumentParser(description='Feed Postgres data into Elastic Search Index')
-parser.add_argument('-i', '--index', required=True, help='Elastic Search index')
-parser.add_argument('-t', '--table', required=True, help='Postgres table')
-parser.add_argument('-wc', '--wordcount', required=False, default=25, help='Minimum number of words per Tweet')
-parser.add_argument('-a', '--attributes', required=False, default=None, help='Comma-separated attributes of Postgres table to be included')
-args = parser.parse_args()                    
+def main():
+    """
+    This script ingests data from an PostgreSQL instance into an Elastic Search index.
+    The processing is based on streaming and arguments can be passed via the command line interface.
+    """
 
-# connect to postgres and elastic search
-es_cred = json.load(open('../es-credentials.json'))
-pg_cred = json.load(open('../pg-credentials.json'))
+    # parse command line arguments
+    parser = argparse.ArgumentParser(description='Feed Postgres data into Elastic Search Index')
+    parser.add_argument('-i', '--index', required=True, help='Elastic Search index')
+    parser.add_argument('-t', '--table', required=True, help='Postgres table')
+    parser.add_argument('--es_credentials', required=False, default='../es-credentials.json', help='Elastic Search credentials file')
+    parser.add_argument('--pg_credentials', required=False, default='../pg-credentials.json', help='Postgres credentials file')
+    parser.add_argument('-wc', '--wordcount', required=False, default=25, help='Minimum number of words per Tweet')
+    parser.add_argument('-a', '--attributes', required=False, default=None, help='Comma-separated attributes of Postgres table to be included')
+    args = parser.parse_args()                    
 
-es_client = es_connect(credentials=es_cred)
-pg_client = pg_connect(credentials=pg_cred)
+    # connect to postgres and elastic search
+    es_cred = json.load(open(file=args.es_credentials))
+    pg_cred = json.load(open(file=args.pg_credentials))
 
-pg_cursor = pg_client.cursor()
+    es_client = es_connect(credentials=es_cred)
+    pg_client = pg_connect(credentials=pg_cred)
 
-if args.attributes is None:
-    # obtain all column names
-    pg_cursor.execute(f"SELECT * FROM {args.table} LIMIT 0")
-    args.attributes = [c[0] for c in pg_cursor.description]
-else:
-    args.attributes = args.attributes.split(",")
+    pg_cursor = pg_client.cursor()
 
-# create index if it not exists
-if not es_client.indices.exists(index=args.index):
-    es_client.indices.create(index=args.index)
+    if args.attributes is None:
+        # obtain all column names
+        pg_cursor.execute(f"SELECT * FROM {args.table} LIMIT 0")
+        args.attributes = [c[0] for c in pg_cursor.description]
+    else:
+        args.attributes = args.attributes.split(",")
 
-# formulate query to add number of words within a tweet text
-word_count_query = (
-    f"SELECT {', '.join(args.attributes)}, array_length(string_to_array(regexp_replace(txt,  '[^\w\s]', '', 'g'), ' '), 1) AS word_count FROM {args.table})"
-)
+    # create index if it not exists
+    if not es_client.indices.exists(index=args.index):
+        print("Creating new index", args.index, "...")
+        es_client.indices.create(index=args.index)
 
-# compose final query
-pg_query = (
-    f"SELECT * FROM ( "
-    f"{word_count_query} AS t " 
-    f"WHERE word_count >= {args.wordcount} "
-)
+    # formulate query to add number of words within a tweet text
+    word_count_query = (
+        f"SELECT {', '.join(args.attributes)}, array_length(string_to_array(regexp_replace(txt,  '[^\w\s]', '', 'g'), ' '), 1) AS word_count FROM {args.table})"
+    )
 
-# execute the query
-print("Executing Postgres Query...")
-pg_cursor.execute(query=pg_query)
+    # compose final query
+    pg_query = (
+        f"SELECT * FROM ( "
+        f"{word_count_query} AS t " 
+        f"WHERE word_count >= {args.wordcount} "
+    )
 
-# insert data from Postgres to ES in a lazy manner
-print("Insert data from Postgres into Elastic Search...")
+    # execute the query
+    print("Executing Postgres Query...")
+    pg_cursor.execute(query=pg_query)
 
-for data in tqdm(iterate(cursor=pg_cursor, attributes=args.attributes, size=1000)):
-    # insert data into ES
-    try:
-        # parse the id to avoid duplicates
-        es_client.index(index=args.index, document=data, id=data["id"])
-    except Exception as exc:
-        print("Exception during data insertion!\n", exc)
-        exit(1)
+    # insert data from Postgres to ES in a lazy manner
+    print("Ingest data from Postgres into Elastic Search...")
+
+    # feed tweets using streaming API
+    progress= tqdm(unit=" tweets")
+    successes = 0
+    for ok, action in streaming_bulk(client=es_client, index=args.index ,actions=iterate(cursor=pg_cursor, attributes=args.attributes, size=10000)):
+        progress.update(1)
+        successes += ok
 
 
-print("Finished feeding process.")
-es_client.close()
-pg_client.close()
+    time.sleep(0.5)
+    print(f"Finished - ingested {successes} tweets")
 
-exit(0)
+    es_client.close()
+    pg_client.close()
+
+    exit(0)
+
+if __name__ == "__main__":
+    main()
